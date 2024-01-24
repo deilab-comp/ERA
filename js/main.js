7dc14dd24912432d9fb76d03033e7049c4bc31e6
Lucas Elvira Martín <lucaselvira96@gmail.com>
2024-01-24 11:35:05 +0100

    Enable the bound effect and generate multiple spheres

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 const scene = document.getElementsByTagName("a-scene")[0];
 const spheres = [];
 
-console.log("hola");
-for (let i = 0; i < 1; i++) {
+for (let i = 0; i < 10; i++) {
   const sphere = document.createElement("a-sphere");
-  sphere.setAttribute("position", {x: 0, y: 2, z: -2});
-  sphere.setAttribute("radius", "0.2");
+  let [x, y, z] = [(Math.random()*5 - 2.5), 3+(Math.random()*2 - 1), -3+(Math.random()*0.5 - 0.25)]
+  let r = Math.random()*0.25 + 0.2;
+  sphere.setAttribute("position", {x, y, z});
+  sphere.setAttribute("radius", r);
   sphere.setAttribute("color", "#FFC65D");
   sphere.setAttribute("gravity", true);
   scene.appendChild(sphere);
--- js/physics.js
+++ js/physics.js
@@ -1,40 +1,52 @@
-let a = -9.8
+const a = -9.8
+const e = 0.9;
 AFRAME.registerComponent("gravity", {
-  schema: {
-    mass: {type: 'number', default: 1},
-    radius: {type: 'number', default: 0.2},
-  },
   init: function() {
+    this.radius = +this.el.getAttribute("radius");
     this.position = new THREE.Vector3();
     this.initialPos = new THREE.Vector3();
-    this.h = this.initialPos.y;
     this.position.copy(this.el.object3D.position);
     this.initialPos.copy(this.el.object3D.position);
+    this.expectedY = this.initialPos.y + 2;
     this.startInterval = 0;
     this.initialSpeed = 0;
+    this.elapsed = 0;
+    this.ready = 0;
+    this.diff = 10;
     setTimeout(() => {
       this.startInterval = new Date();
       this.ready = true;
     }, 3000);
   },
 
-  tick: function() {
-    if (!this.ready) return;
-    if (this.el.object3D.position.y <= 0 || this.el.object3D.position.y > this.initialPos) {
-      this.initialSpeed = Math.pow(2*a*this.initialPos.y, 0.5)
-      a *=-1;
+  tick: function(time, timeDelta) {
+    if (!this.ready || this.diff < 1e-4) return;
+    if (this.el.object3D.position.y <= this.radius) {
+      let speed = Math.pow(Math.abs(2*a*this.initialPos.y), 0.5);
+      this.expectedY = Math.pow(e, 2)*this.initialPos.y - this.radius;
+      this.initialSpeed = e*speed;
       this.startInterval = new Date();
+      this.initialPos.y = this.radius;
+      this.elapsed = 0;
+      if (this.expectedY < this.radius) {
+        this.diff = 0;
+        return;
+      }
+    } else if (this.el.object3D.position.y >= this.expectedY) {
+      this.initialSpeed = 0;
+      this.initialPos.y = this.el.object3D.position.y;
+      this.elapsed = 0;
+      this.expectedY += 2;
     }
-    const elapsed = (new Date() - this.startInterval)/1000
+    this.elapsed+= timeDelta
     //if (elapsed > 3) return;
-    console.log(this.initialSpeed);
-    let distance = getDistance(this.initialPos.y, this.initialSpeed, elapsed);
+    let distance = getDistance(this.initialPos.y, this.initialSpeed, this.elapsed/1000);
+    this.diff = Math.abs(this.el.object3D.position.y - distance);
     this.el.object3D.position.set(this.position.x, distance, this.position.z);
-    
   }
 });
 
 
 function getDistance(high, speed, time) {
-  return high + speed*time + 0.5 * a * time**2
+  return high + speed*time + 0.5 * a * Math.pow(time, 2);
 }
